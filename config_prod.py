ac8fdb0a43be632c8bab0889a24b23b5b1bfd416
Kyle <kyleg@ksdot.org>
2019-08-28 14:51:24 -0500

    Add files via upload

---
+++ config_prod.py
@@ -0,0 +1,9 @@
+'''
+Created on Jun 18, 2019
+
+@author: kyleg
+'''
+
+QC =  r'C:\gisdata\DBConnections\Khub\Khub\qc@KHTransKHUB.sde'
+sde = r'C:\gisdata\DBConnections\Khub\Khub\sde@KHTransKHUB.sde'
+tds = r'C:\gisdata\DBConnections\Khub\Khub\TDSUser@KHProd.sde'
