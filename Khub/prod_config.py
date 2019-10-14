7d85c304070e73ab384431126cec4c24389a5c20
Kyle <kyleg@ksdot.org>
2019-10-14 10:55:58 -0500

    create prodconfig
    database connections mostly

---
+++ Khub/prod_config.py
@@ -0,0 +1,18 @@
+'''
+Created on Sep 10, 2019
+
+@author: kyleg
+'''
+'''
+Created on Jun 18, 2019
+
+@author: kyleg
+'''
+
+QC =  r'C:\Users\kyleg\gisdata\DbConnections\Prod\qc@KHTransKHUB.sde'
+sde = r'C:\Users\kyleg\gisdata\DbConnections\Prod\sde@KHTransKHUB.sde'
+tds = r'C:\Users\kyleg\gisdata\DbConnections\Prod\TDSUser@KHProd.sde'
+rh = r'C:\Users\kyleg\gisdata\DbConnections\Prod\rh@KHTransKHUB.sde'
+pub = r'C:\Users\kyleg\gisdata\DbConnections\Prod\geo@KHPubDev.sde'
+
+layerlist = ''
