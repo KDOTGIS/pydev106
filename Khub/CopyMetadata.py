c2b6558e25158f4d013147f9e0fc10620029468f
Kyle <kyleg@ksdot.org>
2019-12-10 16:21:13 -0600

    update CopyMetadata.py
    as run on AR55

--- Khub/CopyMetadata.py
+++ Khub/CopyMetadata.py
@@ -1,8 +1,8 @@
 '''
 Created on Aug 13, 2019
 Metadata Export from Dev and import to TEST, check. then then run on Prod
-takes about two hours to run on all feature classes, tables, and views
-
+takes about 20 minutes to run on all listed feature classes in dev test and prod
+version as deployed to AR55
 @author: kyleg
 '''
 
@@ -13,55 +13,59 @@ print ("started")
 from arcpy import  ListFeatureClasses, env, ListTables, MetadataImporter_conversion, ExportMetadata_conversion, XSLTransform_conversion, ClearWorkspaceCache_management
 from KhubFCList import EventList, TestEvents
 print ("arcpy tools imported")
-DEV = r'C:\gisdata\DBConnections\Khub\KHDev\rh@KHubdevKHUB.sde'
-DEVNO = 'KHUB.RH.'
-#TARGET = r"C:\gisdata\DBConnections\Khub\KHTest\rh@KHTestKHUB.sde"
-#TARGET = r"C:\gisdata\DBConnections\Khub\Khub\rh@KHTransKHUB.sde"
-TARGET = DEV
-TargetDBNO = 'KHUB.RH.'
-Translator = r"C:/Program Files (x86)/ArcGIS/Desktop10.6/Metadata/Translator/ARCGIS2ISO19139.xml"
+MetaDataSorceDB = r'C:\Users\planadm\GISDATA\DbConnections\KHDev\rh@KHDevKHUB.sde'
+
+DevPub = r'C:\Users\planadm\GISDATA\DbConnections\KHDev\Geo@KHPubDev.sde'
+ProdPub =r'C:\Users\planadm\GISDATA\DbConnections\Prod\Geo@KHPub.sde'
+TestPub = r'C:\Users\planadm\GISDATA\DbConnections\KHTest\Geo@KHPubTest.sde'
+TransTest = r"C:\Users\planadm\GISDATA\DbConnections\KHTest\rh@KHTestKHUB.sde"
+TransProd = r"C:\Users\planadm\GISDATA\DbConnections\Prod\rh@KHTransKHUB.sde"
+TransDBDO = 'KHUB.RH.'
+PubDBNO = 'KHUBPub.Geo.'
+Translator = r"D:/Program Files (x86)/ArcGIS/Desktop10.6/Metadata/Translator/ARCGIS2ISO19139.xml"
 env.overwriteOutput = 1
 
-def exportFromDev():
-    env.workspace = DEV
+def exportFromTransDev():
+    #Transactional DEV is where we are maintaining our comprehensive metadata process, where Chris is actively editing
+    env.workspace = MetaDataSorceDB
     print env.workspace
     print("workspace set")
     #just listing these feature classes and tables takes a pretty long time, like about 20 minutes.
     #features exported for 22 min, then tables took another hour plus
     #FCList = ListFeatureClasses("*KHUB.RH.ev_*")
     for FC in EventList:
-        FCPath = DEVNO + FC
-        print(FC)
-        ExportMetadata_conversion(FC, Translator, r"C:\temp\Khub_Metadata\\"+FC+".xml")
-        MetadataImporter_conversion(FC, r"C:\temp\Khub_Metadata\\"+FC+".xml")
-    
-    #Tables = ListTables()
-    #for table in Tables:
-    #    print(table)
-    #    MetadataImporter_conversion(FC, translator, "C:/temp/Khub_Metadata/"+table+".XML")
-        
+        FCPath = TransDBDO + FC
+        print(FCPath)
+        ExportMetadata_conversion(FCPath, Translator, r"C:\temp\Khub_Metadata\\"+FC+".xml")
+        MetadataImporter_conversion(FCPath, r"C:\temp\Khub_Metadata\\"+FC+".xml")
+                
 def MetadataScrubber():
     print ("Scrubbing Metadata GP history for:")
-    remove_gp_history_xslt = r"C:\Program Files (x86)\ArcGIS\Desktop10.6\Metadata\Stylesheets\gpTools\remove geoprocessing history.xslt"
-    merge_Description2Fields = r"C:/Program Files (x86)/ArcGIS/Desktop10.6/Metadata/Stylesheets/gpTools/merge imported metadata with existing.xslt"
+    remove_gp_history_xslt = r"D:\Program Files (x86)\ArcGIS\Desktop10.6\Metadata\Stylesheets\gpTools\remove geoprocessing history.xslt"
+    #merge_Description2Fields = r"D:/Program Files (x86)/ArcGIS/Desktop10.6/Metadata/Stylesheets/gpTools/merge imported metadata with existing.xslt"
     for FC in EventList:
         print(FC)
-        XSLTransform_conversion(r"C:/temp/Khub_Metadata/"+TargetDBNO+FC+".xml", remove_gp_history_xslt, r"C:/temp/Khub_Metadata/"+TargetDBNO+FC+"Clean.xml", "")
+        XSLTransform_conversion(r"C:/temp/Khub_Metadata/"+FC+".xml", remove_gp_history_xslt, r"C:/temp/Khub_Metadata/"+FC+"Clean.xml", "")
         #XSLTransform_conversion(r"C:/temp/Khub_Metadata/"+TargetDBNO+FC+"Clean.xml", merge_Description2Fields, r"C:/temp/Khub_Metadata/"+TargetDBNO+FC+"Valid.xml", r"C:\temp\Khub_Metadata\styletemplate\KhubTemplate.xml")
         #XSLTransform_conversion(r"C:/temp/Khub_Metadata/"+TargetDBNO+FC+".xml", merge_Description2Fields, r"C:/temp/Khub_Metadata/"+TargetDBNO+FC+"ValidHistory.xml", r"C:\temp\Khub_Metadata\styletemplate\KhubTemplate.xml")
 
-def importTarget():
-    env.workspace = TARGET
+def importTarget(target):
+    env.workspace = target
     print env.workspace
     print("importing metadata")
     for FC in EventList:
         print(FC)
-        MetadataImporter_conversion(r"C:/temp/Khub_Metadata/"+TargetDBNO+FC+"ValidHistory.xml", FC)
+        MetadataImporter_conversion(r"C:/temp/Khub_Metadata/"+FC+"Clean.xml", FC)
+    ClearWorkspaceCache_management()
     
 def main():
-    #exportFromDev()
-    MetadataScrubber()
-    importTarget()
+    exportFromTransDev()  #do this on a routine, deployed with publication upddates
+    MetadataScrubber()  # and do this
+    importTarget(DevPub)
+    importTarget(TestPub)
+    importTarget(ProdPub)
+    importTarget(TransTest)
+    importTarget(TransProd)
     ClearWorkspaceCache_management()
     
 main()
