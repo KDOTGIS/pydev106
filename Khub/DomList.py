396eedc56d5f8e827bafe1f0e3684255e8b107c5
Kyle <kyleg@ksdot.org>
2020-04-09 09:56:20 -0500

    Create DomList.py
    created in git and added text from actual code

---
+++ Khub/DomList.py
@@ -0,0 +1,22 @@
+'''
+created by Kyleg
+4/7/2020
+This is a dictionary with dominant routes and a measure within the bounds of the begin end segments that can be used to query and 
+override route dominance in the route concurrency table for publication of primary routes
+
+'''
+
+OverrideDict = {
+'07830001401':'18.0', 
+'10320007501':'0.85',
+'02820005001':'12.8',
+'10520002401':'2.0',
+'05720005001':'13.68',
+'011200069A1':'5.77',
+'06320007501':'34.0',
+'00620005401':'22.1',
+'08130001801':'11.9',
+'06320016001':'27.2',
+'01120006901':'30.41',
+'105200024C101':'0.31'
+}
